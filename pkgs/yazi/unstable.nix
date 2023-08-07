383dee728be34847f482637a5cda237d3b16aa9c
XYenon <i@xyenon.bid>
2023-08-07 11:02:46 +0800

    yazi-unstable: init at unstable-2023-08-06

---
+++ pkgs/yazi/unstable.nix
@@ -0,0 +1,106 @@
+{ rustPlatform
+, fetchFromGitHub
+, lib
+
+, withFile ? true
+, file
+, withJq ? true
+, jq
+, withPoppler ? true
+, poppler_utils
+, withUnar ? true
+, unar
+, withFfmpegthumbnailer ? true
+, ffmpegthumbnailer
+, withFd ? true
+, fd
+, withRipgrep ? true
+, ripgrep
+, withFzf ? true
+, fzf
+, withZoxide ? true
+, zoxide
+
+, stdenv
+, Foundation
+
+, unstableGitUpdater
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "yazi";
+  version = "unstable-2023-08-06";
+
+  src = fetchFromGitHub {
+    owner = "sxyazi";
+    repo = pname;
+    rev = "badcf994163485b7746902aa70c3e833e9604928";
+    hash = "sha256-sjeX71X4MKHbD4x90Fp1aNsedt5rulaZdN97K9ELdlk=";
+  };
+
+  postPatch =
+    lib.optionalString withFile
+      ''
+        substituteInPlace core/src/external/file.rs \
+          --replace '"file"' '"${file}/bin/file"'
+      ''
+    + lib.optionalString withJq
+      ''
+        substituteInPlace core/src/external/jq.rs \
+          --replace '"jq"' '"${jq}/bin/jq"'
+      ''
+    + lib.optionalString withPoppler
+      ''
+        substituteInPlace core/src/external/pdftoppm.rs \
+          --replace '"pdftoppm"' '"${poppler_utils}/bin/pdftoppm"'
+      ''
+    + lib.optionalString withUnar
+      ''
+        substituteInPlace core/src/external/lsar.rs \
+          --replace '"lsar"' '"${unar}/bin/lsar"'
+        substituteInPlace core/src/external/unar.rs \
+          --replace '"unar"' '"${unar}/bin/unar"'
+      ''
+    + lib.optionalString withFfmpegthumbnailer
+      ''
+        substituteInPlace core/src/external/ffmpegthumbnailer.rs \
+          --replace '"ffmpegthumbnailer"' '"${ffmpegthumbnailer}/bin/ffmpegthumbnailer"'
+      ''
+    + lib.optionalString withFd
+      ''
+        substituteInPlace core/src/external/fd.rs \
+          --replace '"fd"' '"${fd}/bin/fd"'
+      ''
+    + lib.optionalString withRipgrep
+      ''
+        substituteInPlace core/src/external/rg.rs \
+          --replace '"rg"' '"${ripgrep}/bin/rg"'
+      ''
+    + lib.optionalString withFzf
+      ''
+        substituteInPlace core/src/external/fzf.rs \
+          --replace '"fzf"' '"${fzf}/bin/fzf"'
+      ''
+    + lib.optionalString withZoxide
+      ''
+        substituteInPlace core/src/external/zoxide.rs \
+          --replace '"zoxide"' '"${zoxide}/bin/zoxide"'
+      '';
+
+  cargoHash = "sha256-E2+y+ALmUc0d+YKTAtjq4qPvxb+ZtkCA19PiJHB4jPk=";
+
+  buildInputs = lib.optionals stdenv.isDarwin [ Foundation ];
+
+  postInstall = ''
+    mv $out/bin/app $out/bin/yazi
+  '';
+
+  passthru.updateScript = unstableGitUpdater { };
+
+  meta = with lib; {
+    description = "Blazing fast terminal file manager written in Rust, based on async I/O";
+    homepage = "https://github.com/sxyazi/yazi";
+    license = licenses.mit;
+    maintainers = with maintainers; [ xyenon ];
+  };
+}
