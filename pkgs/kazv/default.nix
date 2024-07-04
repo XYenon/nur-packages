e4774fcfc5402331866fdbef9dfb829d6953d293
XYenon <i@xyenon.bid>
2024-07-04 04:31:09 +0000

    kazv: 0.3.0-unstable-2024-06-09 -> 0.3.0-unstable-2024-07-03

--- pkgs/kazv/default.nix
+++ pkgs/kazv/default.nix
@@ -23,15 +23,15 @@
 
 stdenv.mkDerivation rec {
   pname = "kazv";
-  version = "0.3.0-unstable-2024-06-09";
+  version = "0.3.0-unstable-2024-07-03";
 
   src = fetchFromGitLab {
     domain = "lily-is.land";
     owner = "kazv";
     repo = pname;
     fetchSubmodules = true;
-    rev = "f11c4872a0144ba09f53f6232332432a39651bd1";
-    hash = "sha256-yAkjxNZ7jFVu81igxgYRhME7jntEBLjcsB7rPJsim+c=";
+    rev = "7d5684194e3b536ef2a44907cfa6caee71fe52e7";
+    hash = "sha256-TKWSvR7zpDmtGao2HJfP63MV+lF2qsWWHyeUI2M6TWs=";
   };
 
   nativeBuildInputs = [
