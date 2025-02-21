1c8515ba84ab3fb18ddc024ff0b86d5a48e32252
XYenon <i@xyenon.bid>
2025-02-21 16:15:57 +0000

    telemikiya: 0-unstable-2025-02-21 -> 0-unstable-2025-02-22

--- pkgs/telemikiya/default.nix
+++ pkgs/telemikiya/default.nix
@@ -7,13 +7,13 @@
 
 buildGoModule {
   pname = "telemikiya";
-  version = "0-unstable-2025-02-21";
+  version = "0-unstable-2025-02-22";
 
   src = fetchFromGitHub {
     owner = "XYenon";
     repo = "TeleMikiya";
-    rev = "b3fe3baae5ea6cd300f5dba2d72f91bf03779ff4";
-    hash = "sha256-XwsSchD8h9SuiLPqkCDZBfn33SgUqdTjuQTZrivtQkE=";
+    rev = "62d949c18847cb3a71d04b0e84cbf7e91baeb52e";
+    hash = "sha256-7YMY/uAxP7vQs8UPiNTn/21MGtz3QUMgpv9v71ycDgE=";
   };
 
   vendorHash = "sha256-87zQuJyQVuCHvUg4x+OuF+S+Lhv9zmjQUEDiqXBhn9Y=";
