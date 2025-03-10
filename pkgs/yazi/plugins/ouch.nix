5a355fe807815a989307b50ebf844ea9b90fe4b9
XYenon <i@xyenon.bid>
2025-03-10 19:41:36 +0800

    yaziPlugins.ouch: update updateScript

--- pkgs/yazi/plugins/ouch.nix
+++ pkgs/yazi/plugins/ouch.nix
@@ -2,7 +2,7 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
-  unstableGitUpdater,
+  nix-update-script,
 }:
 
 stdenvNoCC.mkDerivation {
@@ -26,7 +26,7 @@ stdenvNoCC.mkDerivation {
     runHook postInstall
   '';
 
-  passthru.updateScript = unstableGitUpdater { tagPrefix = "v"; };
+  passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
 
   meta = with lib; {
     description = "A Yazi plugin to preview archives";
