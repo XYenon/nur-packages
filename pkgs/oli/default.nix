d0f550c6b65b386fa2520059713d55211298a9f8
XYenon <i@xyenon.bid>
2025-07-17 08:52:23 +0000

    oli: 0.53.3 -> 0.54.0

--- pkgs/oli/default.nix
+++ pkgs/oli/default.nix
@@ -2,23 +2,34 @@
   lib,
   rustPlatform,
   fetchgit,
+  runtimeShell,
   nix-update-script,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "oli";
-  version = "0.53.3";
+  version = "0.54.0";
 
   # Cannot use `fetchFromGitHub` because `bin` is marked as `export-ignore`.
   src = fetchgit {
     url = "https://github.com/apache/opendal.git";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-3w0tp8UGkfYQ+6HpKMKTQr0h86wI4z0N/ILtq19dqSM=";
+    hash = "sha256-H+yMEbszuW0J5KlqQd2mHlXYUoyDjGwNkF48JHf4uDU=";
   };
   sourceRoot = "${finalAttrs.src.name}/bin/oli";
 
+  postPatch = ''
+    substituteInPlace tests/integration/edit.rs --replace-warn '#!/bin/bash' '#!${runtimeShell}'
+  '';
+
   useFetchCargoVendor = true;
-  cargoHash = "sha256-NQQPj8w4eoic5fn/VSVe5OOGoj07jUXcTSFZN/q4Zoc=";
+  cargoHash = "sha256-6H0tZ4n1Ft90RtqqM3aj6Id163iij1SSR7wD2hNagb8=";
+
+  checkFlags = [
+    "--skip=edit::test_edit_file_content_replacement"
+    "--skip=edit::test_edit_existing_file_with_changes"
+    "--skip=edit::test_edit_new_file"
+  ];
 
   passthru.updateScript = nix-update-script { };
 
