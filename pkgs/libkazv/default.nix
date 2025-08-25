d2847c1cd434f6c5de4e9175ab4d3d9871825044
XYenon <i@xyenon.bid>
2025-08-25 02:55:16 +0000

    libkazv: 0.8.0-unstable-2025-08-10 -> 0.8.0-unstable-2025-08-24

--- pkgs/libkazv/default.nix
+++ pkgs/libkazv/default.nix
@@ -20,14 +20,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libkazv";
-  version = "0.8.0-unstable-2025-08-10";
+  version = "0.8.0-unstable-2025-08-24";
 
   src = fetchFromGitLab {
     domain = "lily-is.land";
     owner = "kazv";
     repo = "libkazv";
-    rev = "95e280de30fc2536f19e8c57c88dce1fbf6e8918";
-    hash = "sha256-pM6GISzypZKfa3qOHeI5EE1/ibD4pKHgbpQqYKAjfgQ=";
+    rev = "d26639b6d0be4651da3d8b7456877e09a783be01";
+    hash = "sha256-K+I6wiabaOHlWN272JSSvczX8kT1ptp3d1LThvadEQA=";
   };
 
   nativeBuildInputs = [
