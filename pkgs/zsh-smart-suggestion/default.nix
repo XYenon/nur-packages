9095125643af09aee145026fd86181f3101f77ce
XYenon <i@xyenon.bid>
2025-12-30 15:14:08 +0800

    zsh-smart-suggestion: 0.1.3 -> 0.1.5

--- pkgs/zsh-smart-suggestion/default.nix
+++ pkgs/zsh-smart-suggestion/default.nix
@@ -9,13 +9,13 @@
 
 buildGoModule rec {
   pname = "zsh-smart-suggestion";
-  version = "0.1.3";
+  version = "0.1.5";
 
   src = fetchFromGitHub {
     owner = "XYenon";
     repo = "smart-suggestion";
     tag = "v${version}";
-    hash = "sha256-m4TfPvNq96PEzJHT2mpNMwZQlNmgke3UJdLuSJ5DXFw=";
+    hash = "sha256-QM4cV1HHkBiyR6koq7ql+YzYr7xelCp2AJt+tIbS1V8=";
   };
 
   vendorHash = "sha256-2mtBTnvxXjQPXO2/FFljkO15fsIu41liyU5FI0UQ82c=";
